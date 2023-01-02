86c338b5a502afe81ba71795cf7b9207976638f8
Aravind Turaga <thegenie3211@gmail.com>
2023-01-02 17:21:16 -0500

    Implemented all but minimax()

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -80,55 +80,112 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if (board[0][0] and board[0][1] and board[0][2] == "X" or 
-       board[1][0] and board[1][1] and board[1][2] == "X" or
-       board[2][0] and board[2][1] and board[2][2] == "X" or 
-       board[0][0] and board[1][0] and board[2][0] == "X" or
-       board[0][1] and board[1][1] and board[2][1] == "X" or
-       board[0][2] and board[1][2] and board[2][2] == "X" or 
-       board[0][0] and board[1][1] and board[2][2] == "X" or 
-       board[2][0] and board[1][1] and board[0][2] == "X"):
+    if ((board[0][0] == "X" and board[0][1] == "X" and board[0][2] == "X") or 
+       (board[1][0] == "X" and board[1][1] == "X" and board[1][2] == "X") or
+       (board[2][0] == "X" and board[2][1] == "X" and board[2][2] == "X") or 
+       (board[0][0] == "X" and board[1][0] == "X" and board[2][0] == "X") or
+       (board[0][1] == "X" and board[1][1] == "X" and board[2][1] == "X") or
+       (board[0][2] == "X" and board[1][2] == "X" and board[2][2] == "X") or 
+       (board[0][0] == "X" and board[1][1] == "X" and board[2][2] == "X") or 
+       (board[2][0] == "X" and board[1][1] == "X" and board[0][2] == "X")):
 
        return "X"
-
-    elif (board[0][0] and board[0][1] and board[0][2] == "O" or 
-       board[1][0] and board[1][1] and board[1][2] == "O" or
-       board[2][0] and board[2][1] and board[2][2] == "O" or 
-       board[0][0] and board[1][0] and board[2][0] == "O" or
-       board[0][1] and board[1][1] and board[2][1] == "O" or
-       board[0][2] and board[1][2] and board[2][2] == "O" or 
-       board[0][0] and board[1][1] and board[2][2] == "O" or 
-       board[2][0] and board[1][1] and board[0][2] == "O"):
+    
+    elif ((board[0][0] == "O" and board[0][1] == "O" and board[0][2] == "O") or 
+       (board[1][0] == "O" and board[1][1] == "O" and board[1][2] == "O") or
+       (board[2][0] == "O" and board[2][1] == "O" and board[2][2] == "O") or 
+       (board[0][0] == "O" and board[1][0] == "O" and board[2][0] == "O") or
+       (board[0][1] == "O" and board[1][1] == "O" and board[2][1] == "O") or
+       (board[0][2] == "O" and board[1][2] == "O" and board[2][2] == "O") or 
+       (board[0][0] == "O" and board[1][1] == "O" and board[2][2] == "O") or 
+       (board[2][0] == "O" and board[1][1] == "O" and board[0][2] == "O")):
        
        return "O"
     
     else:
         return None
 
-sol = winner(
-    [["X", "O", "O"],
-    [EMPTY, "X", EMPTY],
-    [EMPTY, EMPTY, "X"]]
-)
-
-print(sol)
-
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    winner_of_board = winner(board)
+    empty_spots_count = 0
+
+    for line in range(len(board)):
+        for item in range(len(board[line])):
+            if board[line][item] == EMPTY:
+                empty_spots_count += 1
+
+    if winner_of_board == "X" or winner_of_board == "O":
+        return True
+
+    elif empty_spots_count == 0:
+        return True
+
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winner_of_board = winner(board)
 
+    if winner_of_board == "X":
+        return 1
+    
+    elif winner_of_board == "O":
+        return -1
+    
+    else:
+        return 0
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    current_player = player(board)
+
+    if current_player == "X":
+        possible_action = actions(board)
+
+        for action in possible_action: 
+            action_to_take = max(action, min_value(result(board, possible_action)))
+
+        return action_to_take
+    
+    elif current_player == "O":
+        possible_action = actions(board)
+
+        for action in possible_action:
+            action_to_take = min(action, max_value(result(board, possible_action)))
+
+        return action_to_take
+
+def min_value(board):
+    is_board_terminal = terminal(board)
+
+    if is_board_terminal == True:
+        return utility(board)
+    
+    v = math.inf
+
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+
+    return v
+
+def max_value(board):
+    is_board_terminal = terminal(board)
+
+    if is_board_terminal == True:
+        return utility(board)
+    
+    v = -math.inf
+
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+
+    return v
